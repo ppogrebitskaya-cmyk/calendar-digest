de8727c806ba3118558ed1c3a0251423faf7ae16
Polina <ppogrebitskaya@gmail.com>
2026-06-02 09:43:57 +0300

    обновление

--- src/types.ts
+++ src/types.ts
@@ -34,7 +34,7 @@ export type EventType =
 export const EVENT_COLORS: Record<EventType, string> = {
   startSale: '#1a73e8',
   startStudy: '#0f9d58',
-  lastCall: '#f4511e',
+  lastCall: '#6abf69',
   priceUp1: '#d50000',
   priceUp2: '#c2185b',
   priceUp3: '#880e4f',
